b6a3c90ec24ab6f09970d2da8105bdd65025166b
Jiahao Liu <jiahao.liu@dubizzle.com>
2016-05-04 12:26:59 +0400

    Setup the main screen

---
+++ app/src/main/java/com/jiahaoliuliu/abtestwithapptimize/ItemDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.jiahaoliuliu.abtestwithapptimize;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class ItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
